4d19009bdf3adad5c9849688590c79f232d2b633
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-06-13 16:34:29 +0200

    update README

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,9 +5,9 @@ import Layout from "../components/Layout";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 };
 export default App;
