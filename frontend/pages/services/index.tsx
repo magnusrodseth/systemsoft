3eca8a9999c07712a92236575d89452c708b22ec
Julian <juliangr@stud.ntnu.no>
2021-06-14 17:13:17 +0200

    Added dynamic route rendering for clients and GraphQL queries for clients

--- frontend/pages/services/index.tsx
+++ frontend/pages/services/index.tsx
@@ -1,18 +1,11 @@
-import Wrapper from "../components/Wrapper";
+import Wrapper from "../../components/Wrapper";
 import React from "react";
 import { withUrqlClient } from "next-urql";
 import createUrqlClient from "utils/createUrqlClient";
-import { useServicesQuery } from "generated/graphql";
 
 interface ServicesProps {}
 
 const Services: React.FC<ServicesProps> = ({}) => {
-  const [result, _] = useServicesQuery();
-
-  const { data, fetching, error } = result;
-
-  console.log(data, fetching, error);
-
   return (
     <div className="grid gap-6 grid-cols-3">
       <Wrapper className="col-start-2 bg-red-200">
