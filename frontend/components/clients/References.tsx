18b2650e48da3faa5fe2d89d0e1ec52f6f57b3be
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-06-15 16:43:47 +0200

    Add references styling

--- frontend/components/clients/References.tsx
+++ frontend/components/clients/References.tsx
@@ -1,5 +1,12 @@
-import { useGetClientReferencesQuery } from "generated/graphql";
+import Wrapper from "components/Wrapper";
+import {
+  useGetClientReferencesQuery,
+  References as IReferences,
+} from "generated/graphql";
 import React from "react";
+import classNames from "utils/classNames";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 interface ReferencesProps {
   clientSlug: string;
@@ -14,19 +21,36 @@ const References: React.FC<ReferencesProps> = ({
 
   const { data, fetching, error } = result;
 
+  const references = data?.references as IReferences[];
+
   if (fetching) return <>Loading...</>;
   if (error) return <>Error...</>;
 
   return (
-    <div>
-      {data?.references?.map((reference, key) => {
-        return (
-          <div key={key}>
-            <h1>{reference?.title}</h1>
-            <p>{reference?.description}</p>
-          </div>
-        );
-      })}
+    <div className="flex justify-center">
+      <div className="w-screen grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 space-6">
+        {references.map((reference) => {
+          return (
+            <Wrapper className="bg-blue-100 w-3/4 p-5 ml-auto mr-auto mb-5 mt-5">
+              <div key={reference.id}>
+                <h1
+                  className={classNames(
+                    "text-center text-3xl font-semibold m-2"
+                  )}
+                >
+                  {reference.title}
+                </h1>
+
+                <ReactMarkdown
+                  remarkPlugins={[gfm]}
+                  className="text-left"
+                  children={reference.description}
+                />
+              </div>
+            </Wrapper>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- frontend/pages/clients/[client].tsx
+++ frontend/pages/clients/[client].tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GetServerSideProps } from "next";
-import { useClientQuery } from "generated/graphql";
+import { Clients, useClientQuery } from "generated/graphql";
 import createUrqlClient from "utils/createUrqlClient";
 import { withUrqlClient, WithUrqlProps } from "next-urql";
 import { useRouter } from "next/dist/client/router";
@@ -15,21 +15,29 @@ const Client: React.FC<WithUrqlProps> = ({ name }) => {
   if (fetching) return <>Loading...</>;
   if (error) return <>Error...</>;
 
-  if (data?.clients?.length == 0) {
+  // Clean up input and use const client for the unique client fetched
+  const gotNoClients = data?.clients?.length === 0;
+  const gotSeveralClients = data?.clients?.length !== 1;
+
+  if (gotNoClients || gotSeveralClients) {
     router.push("/clients");
     return <></>;
   }
 
+  const clients = data?.clients as Clients[];
+  const client = clients[0];
+
   return (
     <div>
-      <div>
-        {data?.clients?.map((client) => (
-          <div key={client?.id}>
-            <h1>{client?.name}</h1>
-          </div>
-        ))}
-      </div>
+      <div className="flex flex-col justify-center">
+        <h1 className="text-center text-7xl font-mono font-bold p-6">
+          {client.name}
+        </h1>
 
+        <p className="font-mono text-md text-center">
+          Take a look at how we've helped {client.name}.
+        </p>
+      </div>
       <References clientSlug={name} />
     </div>
   );
