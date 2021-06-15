36f78078f847141346d3ece4adc08e3b2531e483
Julian <juliangr@stud.ntnu.no>
2021-06-15 09:42:19 +0200

    Added redirects for invalid queries

--- frontend/pages/clients/[client].tsx
+++ frontend/pages/clients/[client].tsx
@@ -3,19 +3,26 @@ import { GetServerSideProps } from "next";
 import { useClientQuery } from "generated/graphql";
 import createUrqlClient from "utils/createUrqlClient";
 import { withUrqlClient, WithUrqlProps } from "next-urql";
+import { useRouter } from "next/dist/client/router";
 
 const Client: React.FC<WithUrqlProps> = ({ name }) => {
   const [result, _] = useClientQuery({ variables: { slug: name } });
+  const router = useRouter();
 
   const { data, fetching, error } = result;
 
   if (fetching) return <>Loading...</>;
   if (error) return <>Error...</>;
 
+  if (data?.clients?.length == 0) {
+    router.push("/clients");
+    return <></>;
+  }
+
   return (
     <div>
-      {data?.clients?.map((client) => (
-        <h2>{client?.name}</h2>
+      {data?.clients?.map((client, key) => (
+        <h2 key={key}>{client?.name}</h2>
       ))}
     </div>
   );
