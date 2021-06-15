2711274a99ccfdef65bdbbc3cab688094ff589fb
Julian Grande <juliangr@stud.ntnu.no>
2021-06-15 09:55:48 +0200

    Feat/clients routing #10 (#15)
    * Add models and GraphQL fetching
    
    * Added dynamic route rendering for clients and GraphQL queries for clients
    
    * update strapi
    
    * Added redirects for invalid queries
    
    Co-authored-by: Magnus Rødseth <magnus.rodseth@gmail.com>

--- frontend/pages/services/index.tsx
+++ frontend/pages/services/index.tsx
@@ -1,4 +1,4 @@
-import Wrapper from "../components/Wrapper";
+import Wrapper from "../../components/Wrapper";
 import React from "react";
 import { withUrqlClient } from "next-urql";
 import createUrqlClient from "utils/createUrqlClient";
