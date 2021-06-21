a04a52a1248d0b0804670bd6b2e424407df845d7
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-06-21 15:08:41 +0200

    Add markdown styling and react syntax highlighter

--- frontend/components/Markdown.tsx
+++ frontend/components/Markdown.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import classNames from "utils/classNames";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface MarkdownProps {
   className?: string;
@@ -12,9 +14,40 @@ const Markdown: React.FC<MarkdownProps> = ({
   children,
 }: MarkdownProps) => {
   const styles = className ? className : "";
+
+  const components = {
+    code({ node, inline, className, children, ...props }: any) {
+      const match = /language-(\w+)/.exec(className || "");
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={match[1]}
+          PreTag="div"
+          children={String(children).replace(/\n$/, "")}
+          {...props}
+        />
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
+  };
+
+  // Slice away underlined text, as Strapi does not work properly
+  let text = "";
+
+  if (typeof children === "string") {
+    text = children.replace("<u>", "");
+    text = text.replace("</u>", "");
+  }
+
   return (
-    <ReactMarkdown className={classNames("markdown", styles)}>
-      {children}
+    <ReactMarkdown
+      components={components}
+      className={classNames("markdown", styles)}
+    >
+      {text}
     </ReactMarkdown>
   );
 };
