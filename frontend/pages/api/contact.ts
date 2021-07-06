66d296d57ee1bb4851fb28b05b7900aaed68bd8c
SystemSoft <noreply@systemsoft.no>
2021-07-06 07:57:40 +0000

    Minor changes

--- frontend/pages/api/contact.ts
+++ frontend/pages/api/contact.ts
@@ -1,20 +1,11 @@
-import nodemailer from "nodemailer";
+
 import { NextApiRequest, NextApiResponse } from "next";
 import { validateContactForm } from "../../utils/validateContactForm";
+const sgMail = require("@sendgrid/mail");
+import dotenv from "dotenv";
+dotenv.config();
 
 const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
-  /* TODO: Change to a real account during production */
-  let testAccount = await nodemailer.createTestAccount();
-
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
 
   const { fullName, email, phone, message } = req.body;
 
@@ -34,37 +25,52 @@ const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  const htmlBody = `
-                  <h1>Contact Form Submission from systemsoft.no</h1>
-                  <p><strong>Name:</strong> ${fullName}</p> 
-                  <p><strong>Email:</strong> ${email}</p>
-                  <p><strong>Phone:</strong> ${phone}</p>
-                  <p><strong>Message:</strong> <br> ${message}</p>
-                `;
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  let info = await transporter.sendMail({
-    from: `"${fullName}" <${email}>`,
-    to: "noreply@systemsoft.no",
+  const htmlBody = `
+    <h1>Contact Form Submission from systemsoft.no</h1>
+    <p><strong>Name:</strong> ${fullName}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Phone:</strong> ${phone}</p>
+    <p><strong>Message:</strong> <br> ${message}</p>
+  `;
+
+  const msg = {
+    to: "er@systemsoft.no", // Change to your recipient
+    from: `noreply@systemsoft.no`, // Change to your verified sender
     subject: "New message - SystemSoft Contact Form",
     text: message,
     html: htmlBody,
-  });
+  };
 
-  if (info.response.includes("250")) {
-    res.status(200).json({
-      success: true,
-      description: "Email sent",
-      messageId: info.messageId,
-    });
-  } else {
-    res.status(400).json({
-      success: false,
-      description: "An error occurred",
-      messageId: "",
-    });
-  }
+  sgMail
+    .send(msg)
+    .then((response) => {
+      if (String(response[0].statusCode).indexOf("2") == 0) {
+        res.status(200).json({
+          success: true,
+          description:
+            "Your enquiry was sent successfully! We will get back to you as soon as possible",
+          messageId: email,
+        });
+      } else {
+        res.status(400).json({
+          success: false,
+          description: "An error occurred. Please try again later",
+          messageId: "",
+        });
+      }
 
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    })
+    .catch((error) => {
+      res.status(400).json({
+        success: false,
+        description: "An error occurred. Please try again later",
+        messageId: "",
+      });
+
+    });
 };
 
 export default sendEmail;
+
