56e71bd8dc694755951d91b7f9f81dbcac3ad866
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-12-16 19:05:17 +0100

    add vercel analytics

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,13 @@
 import AppProvider from "@/providers/AppProvider";
+import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <AppProvider>
       <Component {...pageProps} />
+
+      <Analytics />
     </AppProvider>
   );
 };
